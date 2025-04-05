ae4f0740cadbab9e1a9bc14cc1270cede4468520
Romuald Brillout <git@brillout.com>
2025-04-05 18:16:59 +0200

    minimal

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,11 @@ import type { UserConfig } from 'vite'
 
 export default {
   root: 'src',
-  cacheDir: '../.vite',
   plugins: [
     vike(),
     vikeSolid()
   ],
   server: {
     port: 3000
-  },
-  build: {
-    target: 'esnext',
-    outDir: '../dist'
   }
 } satisfies UserConfig
